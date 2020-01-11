0941afbb47d9b7221a8033ab8a9889f0fc598f82
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-11 11:29:21 -0800

    Update HelloWorld.cs

--- Controllers/HelloWorld.cs
+++ Controllers/HelloWorld.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace MvcAdv.Controllers
 {
     public class HelloWorldController : Controller
@@ -8,9 +17,9 @@ namespace MvcAdv.Controllers
             return "This is my default action...";
         }
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}. NumTimes is: {numTimes}");
         }
     }
 }
