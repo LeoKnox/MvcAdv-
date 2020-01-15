6444b5c140fccfdbb93816dbc47de8fcc0e1cec0
LeoKnox <LeoKnox@users.noreply.github.com>
2020-01-14 20:24:21 -0800

    Create Room.cs

---
+++ Models/Room.cs
@@ -0,0 +1,10 @@
+namespace MvcAdv.Models
+{
+    public class Room
+    {
+        public int RoomID { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Width { get; set; }
+    }
+}
